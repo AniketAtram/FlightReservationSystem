5777b723e36babdb19e879f098fa985bfcb960cd
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-12-07 18:38:04 +0530

    Created Ticket class and added attributes and methods

---
+++ src/Ticket.java
@@ -0,0 +1,52 @@
+public class Ticket {
+
+
+    // Attributes of Ticket class
+    int pnrNumber;
+    String departureLocation;
+    String destinationLocation;
+    String departureDate;
+    String departureTime;
+    int seatNumber;
+    float ticketPrice;
+    boolean isCancelled = false;
+    Passenger passengerDetails;
+    Flight flightDetails;
+
+
+    // Constructor for Ticket class
+
+    public Ticket(int pnrNumber, String departureLocation, String destinationLocation, String departureDate, String departureTime, int seatNumber, float ticketPrice, boolean isCancelled, Passenger passengerDetails, Flight flightDetails) {
+        this.pnrNumber = pnrNumber;
+        this.departureLocation = departureLocation;
+        this.destinationLocation = destinationLocation;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.seatNumber = seatNumber;
+        this.ticketPrice = ticketPrice;
+        this.isCancelled = isCancelled;
+        this.passengerDetails = passengerDetails;
+        this.flightDetails = flightDetails;
+    }
+
+
+    // Methods for Ticket class
+
+
+    //Check the status of flight
+    public String checkFlightStatus(Flight flight){
+
+    }
+
+
+    // Check the duration of flight
+    public String checkFlightDuration(Flight flight){
+
+    }
+
+
+    // Cancel ticket
+    public void cancelFlightTicket(){
+        this.isCancelled = true;
+    }
+}
